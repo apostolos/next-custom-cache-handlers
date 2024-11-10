8b15a168f5a288749d17eb35b83caccfdf39d9fd
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2024-11-10 12:24:37 +0200

    feat: enable DIO & add custom data cache

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,25 @@
-/** Add your relevant code here for the issue to reproduce */
+import { Suspense } from 'react';
+import { unstable_cacheLife as cacheLife } from 'next/cache';
+
+async function getData() {
+  'use cache';
+  cacheLife({
+    expire: 10,
+  });
+  return Math.random();
+}
+
+async function AsyncComp() {
+  let data = await getData();
+  return <p>{data}</p>;
+}
+
 export default function Home() {
-  return null;
+  return (
+    <main>
+      <Suspense fallback={<p>Loading...</p>}>
+        <AsyncComp />
+      </Suspense>
+    </main>
+  );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,18 @@
+import path from 'node:path';
+
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  reactStrictMode: true,
+  cacheMaxMemorySize: 0,
+  experimental: {
+    dynamicIO: true,
+    cacheHandlers: {
+      default: path.join(import.meta.dirname, 'lib/debug.mjs'),
+      remote: path.join(import.meta.dirname, 'lib/debug.mjs'),
+      static: path.join(import.meta.dirname, 'lib/debug.mjs'),
+    },
+  },
 };
 
 export default nextConfig;
